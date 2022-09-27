d154a83f31d75407a6d87cf5a720f831b1b63da3
Lidianeangelo <69389376+Lidianeangelo@users.noreply.github.com>
2022-09-27 15:07:38 -0300

    Update exercicio_Vetor_4.java

--- vetores_matrizes/exercicio_Vetor_4.java
+++ vetores_matrizes/exercicio_Vetor_4.java
@@ -36,3 +36,35 @@ public class exercicio_Vetor_4 {
 
 } 
 
+/*3- Leia uma matriz 3 x 3, conte e escreva quantos valores maiores que 10 ela possui.*/
+package exercicios_Vetores_Matrizes;
+
+import java.util.Scanner;
+
+public class exercicio_Matriz {
+
+	public static void main(String[] args) {
+
+		int[][] numero = new int[3][3];
+		int linha = 0, coluna = 0; //contagem = 0;
+
+		Scanner leia = new Scanner(System.in);
+
+		for (linha = 0; linha < 3; linha++) {
+			for (coluna = 0; coluna < 3; coluna++) {
+
+				System.out.println("\nEntre com um número na linha: " + linha + " e coluna: " + coluna + " : ");
+				numero[linha][coluna] = leia.nextInt();
+
+				/*if (numero[linha][coluna] > 10) {
+					contagem++;
+					System.out.println("aqui" + contagem);
+				}*/
+
+			}
+
+		}
+
+	}
+}
+
